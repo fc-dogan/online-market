e42fa4ef2b1962c1ceca4e7fcc2fa55d57ef7cd5
fatma dogan and Matt Taylor <matt@>
2020-01-15 15:50:03 -0800

    add alternating forms

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
     $("#choc").show();
     $("#address-col").show();
   });
+  
+  $("#details").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var addressInput = $("input#address").val();
 
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".address").text(addressInput);
+
+    $("#output").show();
+
+    event.preventDefault();
+  });
 });
